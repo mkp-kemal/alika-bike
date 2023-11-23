eb5f357a7f9eaed0963d53ac8d0dd1f02f5ccbf8
mkp-kemal <kemal.garut7@gmail.com>
2023-11-23 17:16:46 +0700

    Fix type shipping

--- src/pages/Pay.js
+++ src/pages/Pay.js
@@ -182,7 +182,7 @@ export function Pay() {
                             </nav>
 
                             {order.length > 0 ? (
-                                shippingInt <= 0 ? (
+                                shippingInt <= 0 || shippingInt == null ? (
                                     <div style={{ textAlign: 'center', marginTop: '50%', color: 'var(--cyan)' }}>
                                         <p>
                                             <svg xmlns="http://www.w3.org/2000/svg" width="150" height="150" viewBox="0 0 24 24"><circle cx="18" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".67" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0" /></circle><circle cx="12" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin=".33" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0" /></circle><circle cx="6" cy="12" r="0" fill="currentColor"><animate attributeName="r" begin="0" calcMode="spline" dur="1.5s" keySplines="0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8;0.2 0.2 0.4 0.8" repeatCount="indefinite" values="0;2;0;0" /></circle></svg>
